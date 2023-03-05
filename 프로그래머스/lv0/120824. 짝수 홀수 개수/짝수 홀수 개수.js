01351f8586a1babf7bfc6b47f75b382e54499cd6
어재오늘 <jaeochoiii0716@inha.edu>
2023-03-05 19:50:32 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+    
+    for(let i=0; i<num_list.length; i++){
+        if(num_list[i] % 2 === 0){
+            even += 1;
+        }else{
+            odd += 1;
+        }
+    }
+    let newArray = [];
+    newArray[0] = even;
+    newArray[1] = odd;
+    return newArray;
+}
