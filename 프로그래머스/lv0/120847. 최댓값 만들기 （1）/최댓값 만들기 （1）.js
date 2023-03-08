27558dd2b29568905eb63f99bc0f0aa25e8736c7
재오 <jaeochoiii0716@inha.edu>
2023-03-08 17:45:54 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,16 +1,4 @@
 function solution(numbers) {
-    let max1 = 0;
-    let max2 = 0;
-    for(let i=0; i<numbers.length; i++){
-        if(max1 < numbers[i]){
-            max1 = numbers[i];
-            numbers.splice(i,1);
-        }
-    }
-    for(let j=0; j<numbers.length; j++){
-        if(max2 < numbers[j]){
-            max2 = numbers[j];
-        }
-    }
-    return max1 * max2;
+    numbers.sort((a,b)=>b-a);
+    return numbers[0] * numbers[1];
 }
