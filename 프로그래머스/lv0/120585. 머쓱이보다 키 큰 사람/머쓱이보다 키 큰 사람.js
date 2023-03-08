91b9bc47f9494b6f4d636c2854764ca973c1edca
재오 <jaeochoiii0716@inha.edu>
2023-03-08 18:16:28 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,9 @@
+function solution(array, height) {
+    let count = 0;
+    for(let i=0; i<array.length; i++){
+        if(height < array[i]){
+            count += 1;
+        }
+    }
+    return count;
+}
