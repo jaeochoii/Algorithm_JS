9e48e7f2107d9a5dec71eecd286283039caadeaa
재오 <jaeochoiii0716@inha.edu>
2023-03-25 17:46:15 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,5 @@
+function solution(score) {
+    let average = score.map(v => (v[0] + v[1])/2);
+    let sorted = average.slice().sort((a,b) => b-a);
+    return average.map(v => sorted.indexOf(v)+1);
+}
