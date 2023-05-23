91ed37fbe4d3369b20a27d297f680af2250868eb
재오 <jaeochoiii0716@inha.edu>
2023-05-23 15:40:52 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let dp = [0, 1, 2];
+    if( n <= 2 ) return dp[n];
+    for( let i = 3; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    return dp[n];
+}
