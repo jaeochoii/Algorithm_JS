ffa6dd4e6922cdd2cae7e471ee64716d8813bcfd
재오 <jaeochoiii0716@inha.edu>
2023-06-02 13:55:17 +0900

    [level 1] Title: 소수 찾기, Time: 20.61 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -3,7 +3,7 @@ function solution(nums) {
     let count = nums-1;
     for (let i=2; i<Math.sqrt(nums); i++)
         if (prime[i])
-            for (let j=i**2; j<=nums; j+=i)
+            for (let j=i*2; j<=nums; j+=i)
                 if (prime[j]) count--, prime[j]=0;
     return count;
 }
