fd75f9a1061bd520227cae07dfa96d675a8bfa25
Choi Jae O <jaeochoiii0716@inha.edu>
2023-12-19 16:05:26 +0900

    [Silver III] Title: 소수 구하기, Time: 228 ms, Memory: 24804 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const  input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const startNum = Number(input.shift());
+const endNum = Number(input.shift());
+
+function isPrime(endNum) {
+    let numberArr = Array(endNum + 1).fill(true).fill(false, 0, 2);
+    let answer = [];
+
+    for(let i = 2; i <= endNum; i += 1) {
+        if(numberArr[i]) {
+            for(let j = i * 2; j <= endNum; j += i) {
+                numberArr[j] = false;
+            }
+        }
+        else continue;
+    }
+
+    for(let i = startNum; i <= endNum; i += 1){
+        if(numberArr[i]) answer.push(i);
+    }
+
+    console.log(answer.join('\n'));
+}
+
+isPrime(endNum);
