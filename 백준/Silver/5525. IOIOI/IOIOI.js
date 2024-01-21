8bd0f031f5a844f8ae0a41762da376744fa63d0b
Choi Jae O <jaeochoiii0716@inha.edu>
2024-01-21 13:01:59 +0900

    [Silver I] Title: IOIOI, Time: 436 ms, Memory: 113112 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -8,11 +8,41 @@ for(let i = 0; i < N; i += 1) {
     str += 'I';
 }
 
-let count = 0;
+function KMPOccurrenceCount(origin, keyword) {
+    let originalArr = origin.split('');
+    let keywordArr = keyword.split('');
+    let failureArr = Array(keywordArr.length).fill(0);
 
-for(let i = 0; i < STRING.length - str.length + 1; i += 1) {
-    const sliceStr = STRING.slice(i, i + str.length);
-    if(sliceStr === str) count += 1;
+    // failure 배열 세팅
+    for (let i = 1, j = 0; i < keywordArr.length; i += 1) {
+        while (j > 0 && keywordArr[i] !== keywordArr[j]) {
+            j = failureArr[j - 1];
+        }
+        if (keywordArr[i] === keywordArr[j]) {
+            j += 1;
+        }
+        failureArr[i] = j;
+    }
+
+    // 비교 및 카운트 시작
+    let count = 0;
+    let j = 0;
+    for (let i = 0; i < originalArr.length; i++) {
+        while (j > 0 && originalArr[i] !== keywordArr[j]) {
+            j = failureArr[j - 1];
+        }
+        if (originalArr[i] === keywordArr[j]) {
+            if (j === keywordArr.length - 1) {
+                // 키워드가 완전히 일치할 때
+                count++;
+                j = failureArr[j];
+            } else {
+                j += 1;
+            }
+        }
+    }
+
+    return count;
 }
 
-console.log(count);
+console.log(KMPOccurrenceCount(STRING, str));
