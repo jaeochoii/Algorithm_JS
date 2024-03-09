1143192f39f1aed308ed0557fde5dd4b81aed51c
Choi Jae O <jaeochoiii0716@inha.edu>
2024-03-09 22:51:46 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,17 @@
+function solution(bridge_length, weight, truck_weights) {
+    let bridge = Array(bridge_length).fill(0);
+    let time = 0;
+    
+    while(bridge.length) {
+        bridge.shift();
+        if(truck_weights.length) {
+            const sumBridge = bridge.reduce((acc, cur) => acc + cur, 0);
+            if(sumBridge + truck_weights[0] <= weight) {
+                bridge.push(truck_weights.shift());
+            }
+            else bridge.push(0);
+        }
+        time += 1;
+    }
+    return time;
+}
